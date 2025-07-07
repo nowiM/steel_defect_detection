bf21675958c4a43fafe9059406b292b91caf193f
KimDoHyeon <ghkdtprhf5@naver.com>
2025-07-08 00:08:06 +0900

    feat: dataset origin learning

--- dataset_original_learning.py
+++ dataset_original_learning.py
@@ -4,21 +4,21 @@ from ultralytics import YOLO
 import torch
 from ultralytics import YOLO
 
-def train_model():
+def learning_model():
     print(torch.cuda.is_available())
     print(torch.__version__)
 
-    model = YOLO('yolo11n.pt')
+    model = YOLO('yolov8n.pt')
     model.train(
-        data='datasets/data.yaml',
-        epochs=30,
+        data='datasets_original/data.yaml',
+        epochs=50,
         imgsz=640,
         batch=16,
-        name='yolo_test_train',
+        name='yolo_dataset_original',
         device='0'
     )
 
 if __name__ == "__main__":
     import multiprocessing
     multiprocessing.freeze_support()
-    train_model()
+    learning_model()
